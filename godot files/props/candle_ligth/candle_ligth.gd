3f7cfb4164faf25542358b19765ff7d5ff581a0a
Your Namepablo <peadamo@gmail.com>
2024-07-18 16:32:26 -0300

    add more props and little adjustments

--- godot files/props/candle_ligth/candle_ligth.gd
+++ godot files/props/candle_ligth/candle_ligth.gd
@@ -5,14 +5,19 @@ extends Node3D
 var time_passed : float = 0.0
 @onready var omni_light_3d : OmniLight3D= $OmniLight3D
 var lamp_move_value = 0.01 
+var random:float
 
+func _ready():
+	random = randf()*1000
+	time_passed+=random
 
 func _process(delta):
 	time_passed += delta/3
 	
 	var sampled_noise = noise.noise.get_noise_1d(time_passed)
 	sampled_noise = abs(sampled_noise)+0.2
-	omni_light_3d.light_energy = sampled_noise
+	omni_light_3d.light_energy = sampled_noise/4
+	
 	var color = noise.color_ramp.sample(sampled_noise)
 	omni_light_3d.light_color = color
 	omni_light_3d.position.x = 0+sampled_noise*lamp_move_value
@@ -22,5 +27,4 @@ func _process(delta):
 	
 	
 	
-	print(sampled_noise)
 	
