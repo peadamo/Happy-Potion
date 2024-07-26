46f2fce741dac7279dab3af67081ffbe453552e1
Your Namepablo <peadamo@gmail.com>
2024-07-25 23:50:29 -0300

    experimenting shadow companion

---
+++ Pirate_softaware_GamJam_godot/shadow_siluete/shadow_siluette.gd
@@ -0,0 +1,8 @@
+extends Node3D
+@export var palyer_camera : Camera3D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#look_at(palyer_camera.global_postion)
+	pass
